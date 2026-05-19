f1f68eec15b2f10d30ecbe47685597d4a1a4ebff
Paul Masamvu <p.masamvu@alustudent.com>
2026-05-19 16:11:22 -0700

    combinations of single numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+putchar(num + 48);
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+
+return (0);
+}
