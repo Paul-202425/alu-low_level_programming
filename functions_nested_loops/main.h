20d7ec93de78e777b9b4abb7a74bfced93a604d7
Paul Masamvu <p.masamvu@alustudent.com>
2026-05-20 14:41:07 -0700

    main.h is now present

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
+/* Prototypes for your functions */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+
+#endif /* MAIN_H */
