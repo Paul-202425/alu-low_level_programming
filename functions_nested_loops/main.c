6f55c34ef71bf52b498bda6e51c0cf26161d279a
Paul Masamvu <p.masamvu@alustudent.com>
2026-05-20 15:14:15 -0700

    checks for lowercase character

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-   print_alphabet_x10();
-       	return (0);
+int r;
+
+r = _islower('H');
+printf("%d\n", r);
+r = _islower('o');
+printf("%d\n", r);
+r = _islower(108); /* ASCII for 'l' */
+printf("%d\n", r);
+return (0);
 }
