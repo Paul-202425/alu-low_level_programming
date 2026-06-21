01aed22555855ac5ad9baeef52757efb9980d6b7
Paul-202425 <p.masamvu@alustudent.com>
2026-06-21 21:51:06 +0200

    corrected the allocation error

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,30 +2,40 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: rows.
- * @height: columns.
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
  *
- * Return: NULL on failure. If width or height is 0 or negative, return NULL
+ * Return: pointer to array, or NULL on failure
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **array = malloc(sizeof(int *) * height);
-	int i, j;
+    int **array;
+    int i, j;
 
-	if (width < 1 || height < 1)
-		return (NULL);
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		array[i] = malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-			return (NULL);
-	}
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			array[i][j] = 0;
-	return (array);
+    if (width < 1 || height < 1)
+        return NULL;
+
+    array = malloc(sizeof(int *) * height);
+    if (array == NULL)
+        return NULL;
+
+    for (i = 0; i < height; i++)
+    {
+        array[i] = malloc(sizeof(int) * width);
+        if (array[i] == NULL)
+        {
+            // Free all previously allocated rows
+            for (j = 0; j < i; j++)
+                free(array[j]);
+            free(array);
+            return NULL;
+        }
+
+        // Initialize row
+        for (j = 0; j < width; j++)
+            array[i][j] = 0;
+    }
+
+    return array;
 }
