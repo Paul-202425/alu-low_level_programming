70aeb418767595bfc556eab26b7c8b9fc14ec96c
Paul-202425 <p.masamvu@alustudent.com>
2026-06-21 22:11:38 +0200

    corrected the allocation error

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,32 +10,32 @@
  */
 int **alloc_grid(int width, int height)
 {
-int **array;
-int i, j;
+    int **array;
+    int i, j;
 
-if (width <= 0 || height <= 0)
-return NULL;
+    if (width <= 0 || height <= 0)
+        return (NULL);
 
-array = malloc(sizeof(int *) * height);
-if (array == NULL)
-return NULL;
+    array = malloc(sizeof(int *) * height);
+    if (array == NULL)
+        return (NULL);
 
-for (i = 0; i < height; i++)
-{
-array[i] = malloc(sizeof(int) * width);
-if (array[i] == NULL)
-{
-/* Free all previously allocated rows */
-for (j = 0; j < i; j++)
-free(array[j]);
-free(array);
-return NULL;
-}
+    for (i = 0; i < height; i++)
+    {
+        array[i] = malloc(sizeof(int) * width);
+        if (array[i] == NULL)
+        {
+            /* Free all previously allocated rows */
+            for (j = 0; j < i; j++)
+                free(array[j]);
+            free(array);
+            return (NULL);
+        }
 
-/* Initialize row */
-for (j = 0; j < width; j++)
-array[i][j] = 0;
-}
+        /* Initialize row */
+        for (j = 0; j < width; j++)
+            array[i][j] = 0;
+    }
 
-return array;
+    return (array);
 }
